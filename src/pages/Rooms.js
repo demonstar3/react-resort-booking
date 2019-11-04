de059db39615630cdfa96347f74bdceedcb750ae
demonstar3 <demonstar335@gmail.com>
2019-11-04 10:20:03 +0100

    nav, banner, hero

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
 
 const Rooms = () => {
-  return <div>Rooms</div>;
+  return (
+    <Hero>
+      <Banner title="our rooms">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Rooms;
