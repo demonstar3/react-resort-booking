6bb7f75febb5c69efbdfd14c132d7bd359a2ad5d
demonstar3 <demonstar335@gmail.com>
2019-11-04 10:34:37 +0100

    services component done

---
+++ src/components/Services.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Title from "./Title";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+
+const Services = () => {
+  const [services, setServices] = useState([
+    {
+      icon: <FaCocktail />,
+      title: "free cocktails",
+      info:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente."
+    },
+    {
+      icon: <FaHiking />,
+      title: "Endless Hiking",
+      info:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente."
+    },
+    {
+      icon: <FaShuttleVan />,
+      title: "Free shuttle",
+      info:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente."
+    },
+    {
+      icon: <FaBeer />,
+      title: "Strongest beer",
+      info:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente."
+    }
+  ]);
+
+  return (
+    <section className="services">
+      <Title title="services" />
+      <div className="services-center">
+        {services.map((item, index) => {
+          return (
+            <article key={index} className="service">
+              <span>{item.icon}</span>
+              <h6>{item.title}</h6>
+              <p>{item.info}</p>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
