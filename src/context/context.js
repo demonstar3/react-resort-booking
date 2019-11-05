93f703a77a50daa9a288817827d8a446f311c9c3
demonstar3 <demonstar335@gmail.com>
2019-11-05 12:24:44 +0100

    added data from contentful

--- src/context/context.js
+++ src/context/context.js
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect } from "react";
-import items from "../data";
+// import items from "../data";
 import useAsyncState from "../utils/useAsyncState";
+import Client from "../contentful";
 
 const RoomContext = createContext();
 
@@ -20,6 +21,36 @@ const RoomProvider = ({ children }) => {
     breakfast: false,
     pets: false
   });
+
+  // getData from contentful
+  const getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: "beachResortRoom",
+        order: "sys.createdAt"
+      });
+
+      let rooms = formatData(response.items);
+      let featuredRooms = rooms.filter(room => room.featured === true);
+      let maxPrice = Math.max(...rooms.map(item => item.price));
+      let maxSize = Math.max(...rooms.map(item => item.size));
+
+      setState(prevState => {
+        return {
+          ...prevState,
+          rooms: rooms,
+          featuredRooms: featuredRooms,
+          sortedRooms: rooms,
+          loading: false,
+          price: maxPrice,
+          maxPrice: maxPrice,
+          maxSize: maxSize
+        };
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
   const formatData = items => {
     let tempItems = items.map(item => {
@@ -49,23 +80,7 @@ const RoomProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    let rooms = formatData(items);
-    let featuredRooms = rooms.filter(room => room.featured === true);
-    let maxPrice = Math.max(...rooms.map(item => item.price));
-    let maxSize = Math.max(...rooms.map(item => item.size));
-
-    setState(prevState => {
-      return {
-        ...prevState,
-        rooms: rooms,
-        featuredRooms: featuredRooms,
-        sortedRooms: rooms,
-        loading: false,
-        price: maxPrice,
-        maxPrice: maxPrice,
-        maxSize: maxSize
-      };
-    });
+    getData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
